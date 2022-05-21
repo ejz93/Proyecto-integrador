60210778a6391003d5904b22a1aa68bff5664301
ejz93 <juarezestanislao93@gmail.com>
2022-05-21 02:58:18 -0300

    Creación Backend

---
+++ Proyecto-integrador/BackEnd/src/main/java/com/portfolio/ejz/EjzApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ejz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjzApplication.class, args);
+	}
+
+}
